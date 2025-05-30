d15faa807fa6cf1c6b25e1b25a4193839d95d90e
YALDA KHOSHPEY <165719263+YALDAKHOSHPEY@users.noreply.github.com>
2025-05-30 19:07:02 +0330

    Add files via upload

---
+++ PSET7/movies/10.sql
@@ -0,0 +1,10 @@
+SELECT name
+FROM people
+WHERE id IN
+    (SELECT DISTINCT directors.person_id
+     FROM directors
+     JOIN movies ON movies.id = directors.movie_id
+     WHERE movie_id IN
+         (SELECT movie_id
+          FROM ratings
+          WHERE rating >= 9));
